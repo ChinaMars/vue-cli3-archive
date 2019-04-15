2ac6a1a53279c3b15c6d350f49fa059e6caf0682
黄玮 <mars.huang@d1m.cn>
2019-04-15 18:46:38 +0800

    vue init

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 
 <style lang="scss">
+  @import "./style/common/variables.scss";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul :class="$style.red">
+      <li w-375-224 aspectratio aspect-ratio="375/224">
+        one
+      </li>
+      <li class="ignore">two</li>
+      <li>three</li>
+      <li>four</li>
+    </ul>
   </div>
 </template>
 
@@ -16,3 +24,22 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  [w-375-224]{
+    width: 375px;
+    z-index: 99;
+  }
+  [w-375-224]{
+    aspect-ratio:'375:224';
+  }
+  .ignore{
+    width: 100px;
+  }
+</style>
+
+<style module>
+  .red{
+    color: red;
+  }
+</style>
